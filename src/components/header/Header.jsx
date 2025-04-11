6d357f8ec92eabf7e3a3ef808a59dacfa3329c86
Ankit Deb <deb.ankit.work@gmail.com>
2025-04-11 16:13:04 +0530

    made changes to mobile screen faults
    updated mobile screen view

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,89 +1,71 @@
 import React, { useState } from 'react';
 import "./header.css";
 
-
-
 const Header = () => {
-
-
-  // for Toggle Menu
   const [Toggle, showMenu] = useState(false);
 
-
-
+  // For closing the menu with a small delay
+  const handleNavClick = () => {
+    setTimeout(() => {
+      showMenu(false);
+    }, 300); // delay of 300ms
+  };
 
   return (
     <header className="header">
       <nav className='nav container'>
-
         <a href="index.html" className="nav__logo">Deb's Portfolio</a>
 
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+              <a href="#home" className="nav__link active-link" onClick={handleNavClick}>
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
 
-
-
-
-
             <li className="nav__item">
-              <a href="#about" className="nav__link">
+              <a href="#about" className="nav__link" onClick={handleNavClick}>
                 <i className="uil uil-user nav__icon"></i> About
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
+              <a href="#skills" className="nav__link" onClick={handleNavClick}>
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="#projects" className="nav__link">
+              <a href="#projects" className="nav__link" onClick={handleNavClick}>
                 <i className="uil uil-briefcase-alt nav__icon"></i> Projects
               </a>
             </li>
 
-
             <li className="nav__item">
-              <a href="#crafts" className="nav__link">
-                <i className="uil uil-scenery nav__icon"></i> Motion Graphics
+              <a href="#crafts" className="nav__link" onClick={handleNavClick}>
+                <i className="uil uil-scenery nav__icon"></i>
+                <span className="desktop-label">Motion Graphics</span>
+                <span className="mobile-label">MoGraphs</span>
               </a>
             </li>
 
-
-
-
-
             <li className="nav__item">
-              <a href="#contact" className="nav__link">
+              <a href="#contact" className="nav__link" onClick={handleNavClick}>
                 <i className="uil uil-message nav__icon"></i> Contact
               </a>
             </li>
-
-
-     
-
-
           </ul>
 
-
-
-          <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
-
+          <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
         </div>
 
         <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
-          <i class="uil uil-apps"></i>
+          <i className="uil uil-apps"></i>
         </div>
-
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
